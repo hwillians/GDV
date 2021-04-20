68e627e5230b7a336ecd777869eef0c28307a51e
hwillians <66992569+hwillians@users.noreply.github.com>
2021-04-20 11:44:55 +0200

    class voiture

--- MesClasses/Vehicules/Camion.cs
+++ MesClasses/Vehicules/Camion.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MesClasses.Vihicules
+﻿namespace MesClasses.Vehicules
 {
     public class Camion : Vehicule
     {
---
+++ MesClasses/Vehicules/Voiture.cs
@@ -0,0 +1,12 @@
+﻿namespace MesClasses.Vehicules
+{
+    public class Voiture : Vehicule
+    {
+        public int Puissance { get; set; }
+        public Voiture(string marque, string modele, int numero, int puissance) : base(marque, modele, numero)
+        {
+            Puissance = puissance;
+        }
+        public override string ToString() => base.ToString() + $"puissance : {Puissance}";
+    }
+}
